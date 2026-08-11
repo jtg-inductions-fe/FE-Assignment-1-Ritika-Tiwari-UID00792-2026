d3b2a073de66971366d9c9af0bbb8171fac368a6
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-08-11 14:37:12 +0530

    [RT_A1_01]: Implement opening animation for hamburger menu.

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,7 @@
+/**
+ * Implementation of opening and closing logic of hamburger using toggle class.
+ */
+
 const hamburger = document.querySelector('.header__hamburger');
 const header = document.querySelector('.header');
 const closingButton = document.querySelector('.navigation__close-btn');
