2e92be517f18c0e28240443babce92521edf7435
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-08-14 00:48:12 +0530

    [RT_A1_01]: Add JS logic to handle hamburger close state while user press esc or click outside the hamburger.

--- src/script/index.js
+++ src/script/index.js
@@ -1,17 +1,40 @@
-/**
- * Implementation of opening and closing logic of hamburger using toggle class.
+/*
+Implementation of opening and closing logic of hamburger using toggle class.
  */
-
 const hamburger = document.querySelector('.header__hamburger');
 const header = document.querySelector('.header');
 const closingButton = document.querySelector('.navigation__close-btn');
 
-header.classList.remove('navigation-open');
-
-hamburger.addEventListener('click', () => {
+/**
+ * Function for toggle navigation open and close state
+ */
+function toggleNav() {
     header.classList.toggle('navigation-open');
+}
+
+/**
+ * Function for closing navigation menu
+ */
+function closeNav() {
+    header.classList.remove('navigation-open');
+}
+
+hamburger.addEventListener('click', toggleNav);
+closingButton.addEventListener('click', closeNav);
+
+document.addEventListener('keypress', (e) => {
+    if (e.key == 'Escape') {
+        closeNav();
+    }
 });
 
-closingButton.addEventListener('click', () => {
-    header.classList.remove('navigation-open');
+document.addEventListener('click', (e) => {
+    if (
+        header.classList.contains('navigation-open') &&
+        !header.contains(e.target)
+    ) {
+        closeNav();
+    }
 });
+
+closeNav();
