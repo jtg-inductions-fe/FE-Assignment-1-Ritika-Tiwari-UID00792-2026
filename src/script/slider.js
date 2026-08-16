888f5cb494ad12bf9f6fc342d9b46d8e897ca60c
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-08-17 02:15:49 +0530

    [RT_A1_04]: Install swiper and add json data for slides and also implement JS logic for slider.

---
+++ src/script/slider.js
@@ -0,0 +1,40 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+
+const testimonials = document.querySelector('.testimonials__list');
+let response = await fetch('./src/script/slides.json');
+let testimonialsData = await response.json();
+
+testimonials.innerHTML = testimonialsData
+    .map(
+        ({ name, role, image, rating, text }) => `
+<article class="testimonials__slides swiper-slide">
+<img src="${image}" alt="${name}" class="testimonials__image"/>
+<h3 class="testimonials__author">
+    <span class="testimonials__name">${name}</span>
+    <span class="testimonials__role">${role}</span>
+</h3>
+<div class="testimonials__rating">${'*'.repeat(rating)}</div>
+<p class="testimonials__description">${text}</p>
+</article>
+`,
+    )
+    .join('');
+const previousBtn = document.querySelector('.testimonials__button--previous');
+const nextBtn = document.querySelector('.testimonials__button--next');
+const pagination = document.querySelector('.testimonials__pagination');
+
+new Swiper('.testimonials__slider', {
+    modules: [Navigation, Pagination],
+    slidesPreView: 1,
+    spaceBetween: 24,
+    navigation: {
+        prevEl: previousBtn,
+        nextEl: nextBtn,
+    },
+    pagination: {
+        el: pagination,
+        clickable: true,
+    },
+});
