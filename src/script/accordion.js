8c4e21e6d18d31ec2d0663948418a85092cb6a50
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-08-19 01:40:08 +0530

    [RT_A1_05]: use index.js as a barrel file.

--- src/script/accordion.js
+++ src/script/accordion.js
@@ -1,9 +1,13 @@
-const footerSection = Array.from(document.querySelectorAll('.footer__section'));
+export function accodion() {
+    const footerSection = Array.from(
+        document.querySelectorAll('.footer__section'),
+    );
 
-// Toggle footer section on button click
-footerSection.forEach((section) => {
-    const button = section.querySelector('.footer__section-header');
-    button.addEventListener('click', () => {
-        section.classList.toggle('footer__section--open');
+    // Toggle footer section on button click
+    footerSection.forEach((section) => {
+        const button = section.querySelector('.footer__section-header');
+        button.addEventListener('click', () => {
+            section.classList.toggle('footer__section--open');
+        });
     });
-});
+}
