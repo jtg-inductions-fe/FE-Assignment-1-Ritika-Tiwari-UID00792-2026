42259601ee3ace80eccb5e831f5711f2bac5730d
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-08-18 15:24:12 +0530

    [RT_A1_05]: Add JS logic for accordian's open and close state using toggle class, and also change the alignment of footer navigation items.

---
+++ src/script/accordian.js
@@ -0,0 +1,9 @@
+const footerSection = Array.from(document.querySelectorAll('.footer__section'));
+
+// Toggle footer section on button click
+footerSection.forEach((section) => {
+    const button = section.querySelector('.footer__section-header');
+    button.addEventListener('click', () => {
+        section.classList.toggle('footer__section--open');
+    });
+});
