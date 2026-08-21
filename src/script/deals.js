f6bc52b6b69260d767489811493d85f15f17ae19
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-08-21 19:15:14 +0530

    [RT_A1_08]: fetch data from the api and apply filter on null values and choosed random 4 items for offer deals data.

--- src/script/deals.js
+++ src/script/deals.js
@@ -2,6 +2,9 @@
 export function Drawer() {
     const overlay = document.body.querySelector('.special-offer');
     const specialDeals = document.getElementById('deals');
+    // const specialOfferConatiner = document.querySelector(
+    //     '.special-offer__container',
+    // );
     const header = document.querySelector('.header__container');
     const closeBtn = document.querySelector('.special-offer__close-btn');
 
@@ -26,9 +29,45 @@ export function Drawer() {
         closeDrawer();
     });
 
+    //fetch api
+    let dealsOnWheel = [];
+    const unloackedDeals = [
+        { label: '20% Off Flights', promoCode: 'FLY20-X8J2', validFor: 13 },
+        { label: 'VIP Lounge', promoCode: 'VIP-LMN9', validFor: 29 },
+    ];
+    async function fetchDeals() {
+        const response = await fetch(
+            'https://gist.githubusercontent.com/ameer-wajid-ali/1f29ebee4295cede36f8d74b45e576df/raw/122966c9a123861249f173911d8d93a76dc06d7a/ ',
+        );
+        const data = await response.json();
+        //filter null values
+        for (let i in data) {
+            if (data[i]['validFor'] == null) {
+                data[i]['validFor'] = 7;
+            }
+        }
+        dealsOnWheel = data.filter(
+            (Item) =>
+                !unloackedDeals.some(
+                    (excludeItem) => excludeItem.label === Item.label,
+                ),
+        );
+        spinWheel(dealsOnWheel);
+    }
+    fetchDeals();
+
     //Spin wheel login in js and dynamically render the items in the wheel
-    function spinWheel() {
-        const items = [1, 2, 3, 4];
+    function spinWheel(dealsOnWheel) {
+        let items = [];
+        let i = 0;
+        while (i < 4) {
+            let item =
+                dealsOnWheel[Math.floor(Math.random() * dealsOnWheel.length)];
+            if (!items.includes(item)) {
+                items.push(item.label);
+                i++;
+            }
+        }
         const wheel = document.querySelector('.special-offer__spinner');
         const spinBtn = document.querySelector('.special-offer__spin-btn');
         const sliceAngle = 360 / items.length;
@@ -51,13 +90,17 @@ export function Drawer() {
             const text = document.createElement('span');
             text.className = 'special-offer__items';
             text.textContent = item;
-            const angle = index * sliceAngle + sliceAngle / 2;
+            const angle = index * sliceAngle;
             // rotate the text to half of slice angle to place it in the center of slice
             text.style.transform = `rotate(${angle}deg) translateY(-80px)`;
             wheel.appendChild(text);
         });
 
+        // let temp = document.getElementsByTagName('template')[0];
+        // let unloackedDealsCard = temp.content.cloneNode(true);
         let currentRotation = 0;
+        // let result;
+
         spinBtn.addEventListener('click', () => {
             const randomIndex = Math.floor(Math.random() * items.length);
             // calcualtion of the target angle of the winner slice from its initial angle
@@ -73,7 +116,8 @@ export function Drawer() {
 
             currentRotation += rotation;
             wheel.style.transform = `rotate(${currentRotation}deg)`;
+            // result = items[randomIndex];
         });
     }
-    spinWheel();
+    // spinWheel();
 }
